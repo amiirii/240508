b67507198fd22820ea64eb14098e632644dd547a
amiirii <37143130+amiirii@users.noreply.github.com>
2019-09-26 19:41:57 +0200

    Update juego.py

--- Practica 1/juego.py
+++ Practica 1/juego.py
@@ -1,6 +1,5 @@
 import random
 
-
 class Jugador:
     def __init__(self, nombre, tipo):
         self.nombre = nombre
@@ -10,41 +9,42 @@ class Jugador:
         if self.tipo == 'm':
             self.numero = random.randrange(0, 100)
         else:
-            self.numero = input('{}, ¿cuál es tu número?'.format(self.nombre))
+            self.numero = input('{}, ¿cuál es tu número?\n'.format(self.nombre))
+
+    def piensam(self,num,ant):
+        r = input('mayor/menor/si\n')
+        if r == 'menor':
+            self.numero = random.randrange(ant, num)
+            return 0
+        elif r == 'mayor':
+            self.numero = random.randrange(num, ant)
+            return 0
+        elif r == 'si':
+            print('Numero correcto')
+            return 1
 
     def comprueba(self, num):
-	if self.tipo == 'm':
-		if num == self.numero:
-		    print('Numero correcto')
-		    return 1
+        if num == self.numero:
+            print('Numero correcto')
+            return 1
 
-		elif num > self.numero:
-		    print('El numero es menor')
-		    return 0
-		else:
-		    print('El numero es mayor')
-		    return 0
-	if self.tipo == 'h':
-		r = input('mayor/menor/si')
-		if r == 'mayor':
-			self.numero = random.randrange(self.numero, 100)
-			return 0
-		elif r == 'menor':
-			self.numero = random.randrange(0, self.numero)
-			return 0
-		elif r == 'si':
-			print('Numero correcto');
-			return 1	
-
+        elif num > self.numero:
+            print('El numero es menor')
+            return 0
+        else:
+            print('El numero es mayor')
+            return 0
 
 if __name__ == '__main__':
     m = 's'
     while m == 's':
         print('Comienza el juego')
+        print('He pensado un numero del 0 al 100, intenta acertarlo')
 
-        i,j = 1
+        i = 1
+        j = 1
 
-        j1 = Jugador(input('Nombre jugador: '), 'h')
+        j1 = Jugador(input('Nombre jugador: \n'), 'h')
         maquina = Jugador('Maquina', 'm')
 
         maquina.piensa()
@@ -53,41 +53,48 @@ if __name__ == '__main__':
             print('** Turno ', i, '**')
 
             j1.piensa()
-
-            r = maquina.comprueba(j1.numero)
+            r = maquina.comprueba(int(j1.numero))
 
             i += 1
 
-        print('Enhorabuena, has acertado el número en ',i, ' intentos')
-	print('Es mi turno, piensa un numero del 1 al 100')
- 	r = 0
+        print('Enhorabuena, has acertado el número en ', i - 1, ' intentos')
+        print('Es mi turno, piensa un numero del 1 al 100')
+
+        maquina.piensa()
+        min = 0
+        max = 100
+        r = 0
+
         while r != 1:
             print('** Turno ', j, '**')
-	    maquina.piensa()
-	    print('¿Es tu numero el ',maquina.numero,'?')
+            print('¿Es tu numero el',maquina.numero,'?')
 
-            r = j1.comprueba(maquina.numero)
+            r = input('mayor/menor/si\n')
+            if r == 'menor':
+                max = maquina.numero
+                if max == maquina.numero:
+                    max = maquina.numero - 1
+                maquina.numero = random.randrange(min, maquina.numero)
+                r = 0
+            elif r == 'mayor':
+                min = maquina.numero
+                if min == maquina.numero:
+                    min = maquina.numero + 1
+                maquina.numero = random.randrange(maquina.numero, max)
+                r = 0
+            elif r == 'si':
+                print('Numero correcto')
+                r = 1
 
-            j += 1	
-	
-	print('He acertado en ',j,' intentos')
-	if j>i:
-		print('Has acertado el numero en menos intentos que yo, enhorabuena, has ganado')
-	elif j == i:	
-		print('Hemos acertado el numero en los mismos intentos, empate')
-	else:
-		print('He acertado tu numero en menos intentos que tu el mio, he vencido')
+            j += 1
 
-
-	
-	
-	
-	
-	
-
-
-	
-	
-
-
-        m = input('¿Quieres jugar otra partida? s/n')
+        print('He acertado en ', j - 1, ' intentos')
+        if j > i:
+            print('Has acertado el numero en menos intentos que yo, enhorabuena, has ganado')
+            m = input('¿Quieres jugar de nuevo? s/n\n')
+        elif j == i:
+            print('Hemos acertado el numero en los mismos intentos, empate')
+            m = input('¿Quieres jugar de nuevo? s/n\n')
+        else:
+            print('He acertado tu numero en menos intentos que tu el mio, he vencido')
+            m = input('¿Quieres jugar de nuevo? s/n\n')
