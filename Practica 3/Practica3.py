c4f3b0a890f1e8736fb809ae127cd8b958864db4
amiirii <37143130+amiirii@users.noreply.github.com>
2019-10-04 13:30:38 +0200

    Update Practica3.py

--- Practica 3/Practica3.py
+++ Practica 3/Practica3.py
@@ -9,15 +9,17 @@ def mostrar_info():
     conn = sqlite3.connect("ejemplo.db")
     query = ("select * from sqlite_master where type='table';")
     c = conn.execute(query)
-    return (c.fetchone())
+    res = c.fetchall()
+    return [res[1] for row in res]
 
 @app.route("/tables/<sr>")
 def mostrar_tablas(sr):
     conn = sqlite3.connect("ejemplo.db")
     query = ("SELECT * FROM " + sr + ";")
     c = conn.execute(query)
-    return (c.fetchone())
+    res = c.fetchall()
+    return [res[1] for row in res]
 
-if __name__ == "__main":
+if __name__ == "__main__":
     app.run()
 
